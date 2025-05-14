6bbb61101d0c872befecf1d429e43158daeed773
mariospeterman <mariospeterman@gmail.com>
2025-05-14 18:24:54 +0200

    Add GitHub Actions workflow and update Astro config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://maki.github.io',
+	base: '/puffy-phase',
 	integrations: [
 		tailwind(),
 		react()
